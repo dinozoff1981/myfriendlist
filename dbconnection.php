d1c41b1373aa987d4309f8c695e32cf6278e8d5d
dinozoff1981 <51277761+dinozoff1981@users.noreply.github.com>
2022-08-28 02:17:06 +0200

    nconnect

--- dbconnection.php
+++ dbconnection.php
@@ -1,10 +1,6 @@
-//<?php
-//$con=mysqli_connect("eu-cdbr-west-03.cleardb.net", "b50d2d6a32b390", "931391db", "heroku_2844bab4cf30724");
-//if(mysqli_connect_errno())
-//{
-//echo "Connection Fail".mysqli_connect_error();
-//}
-//?>
+
+
+
 
 <?php
 //Get Heroku ClearDB connection information
