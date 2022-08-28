6b169c6c17ff41402088c32221cb1ebeed4eeff2
dinozoff1981 <51277761+dinozoff1981@users.noreply.github.com>
2022-08-28 02:58:55 +0200

    retry

--- create.php
+++ create.php
@@ -1,10 +1,10 @@
 <?php 
 
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_server = $cleardb_url["cdbr-west-03.cleardb.net"];
+$cleardb_username = $cleardb_url["b50d2d6a32b390"];
+$cleardb_password = $cleardb_url["931391db"];
+$cleardb_db = substr($cleardb_url["heroku_2844bab4cf30724"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
--- edit.php
+++ edit.php
@@ -1,15 +1,14 @@
 <?php 
 
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"],1);
+$cleardb_server = $cleardb_url["cdbr-west-03.cleardb.net"];
+$cleardb_username = $cleardb_url["b50d2d6a32b390"];
+$cleardb_password = $cleardb_url["931391db"];
+$cleardb_db = substr($cleardb_url["heroku_2844bab4cf30724"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
 if(isset($_POST['submit']))
   {
   	$eid=$_GET['editid'];
