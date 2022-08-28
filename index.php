ce0773bb5c7d715f740e6c086d4241b44b2496e8
dinozoff1981 <51277761+dinozoff1981@users.noreply.github.com>
2022-08-28 02:04:20 +0200

    dbconn

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
-//database conection  file
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+
 include('dbconnection.php');
-//Code for deletion
+
 if(isset($_GET['delid']))
 {
 $rid=intval($_GET['delid']);
