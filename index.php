97e2bfe1549f7524e7924de830bb39fddef86745
dinozoff1981 <51277761+dinozoff1981@users.noreply.github.com>
2022-09-12 18:21:16 +0200

    newname

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include 'connect.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>My friends</title>
+    <title>Shalom Travel</title>
 
 </head>
 
